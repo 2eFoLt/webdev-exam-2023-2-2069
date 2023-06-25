0a58b4c84382d21d97667f1031dddabefa4e03de
2eFoLt <grig2205@gmail.com>
2023-06-25 19:14:30 +0300

    update_CRUD_userpolicy

---
+++ app/users_policy.py
@@ -0,0 +1,30 @@
+from flask_login import current_user
+
+
+class UsersPolicy:
+    def __init__(self, record):
+        self.record = record
+
+    def assign_role(self):
+        return current_user.is_admin()
+
+    def show(self):
+        if current_user.is_admin():
+            return True
+        if str(current_user.id) == str(self.record.id):
+            return True
+        return False
+
+    def delete(self):
+        return current_user.is_admin()
+
+    def create(self):
+        return current_user.is_admin()
+
+    def edit(self):
+        if current_user.is_moderator() or current_user.is_admin():
+            return True
+        return False
+
+    def show_stat(self):
+        return current_user.is_admin()
